6f5f70aeff2adf9bf9e9d778542fc13722d1de36
Atila-ms <moyasantuchoatila@gmail.com>
2025-11-11 19:00:25 -0300

    Update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+<<<<<<< Updated upstream
+=======
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+>>>>>>> Stashed changes
